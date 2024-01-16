d00108c8f5b37ee3b2b760245208493f5ee8a641
Beni Stocker <benjamin.stocker@gmail.com>
2024-01-16 20:59:56 +0100

    added biome map creation properly

--- analysis/plot_map_biomes.R
+++ analysis/plot_map_biomes.R
@@ -1,35 +1,39 @@
-# library(sf)
-# library(remotes)
-# install_github("r-tmap/tmaptools")
-# install_github("r-tmap/tmap")
-
+library(sf)
+library(terra)
 library(dplyr)
-# library(tmap)
-# library(tmaptools)
 library(ggplot2)
 library(readr)
 
-# # biomes <- sf::read_sf("~/data/biomes/wwf_ecoregions/official/wwf_terr_ecos.shp")
-# biomes <- sf::read_sf("~/Downloads/terr-ecoregions-TNC/tnc_terr_ecoregions.shp")
-# 
-# rasta <- terra::rast(
-#   here::here("data/modis_landcover__LPDAAC__v5.1__0.1deg__2010.nc")
-#   )
-# 
-# biomes_raster <- terra::rasterize(biomes, rasta, field = "WWF_MHTNAM")
-# 
-# coast <- rnaturalearth::ne_coastline(scale = 110, returnclass = "sf")
-# 
-# sites <- read_rds(here::here("data/df_sites_sub.rds")) |>
-#   sf::st_as_sf(coords = c("lon", "lat"), crs = sf::st_crs(biomes))
+sf::sf_use_s2(FALSE)
 
+# Biomes shapefile -------------
+# Get data from Olson, D. M, 2020, "Terrestrial ecoregions of the world (Copy to use in GapAnalysis R package)", 
+# https://doi.org/10.7910/DVN/WTLNRG, Harvard Dataverse, V1
+biomes <- sf::read_sf("~/data/biomes/olson_harvard_dataverse/tnc_terr_ecoregions.shp")
 
+# Rasterise ---------------
+# Load template raster available in this repository
+rasta <- terra::rast(
+  here::here("data/modis_landcover__LPDAAC__v5.1__0.1deg__2010.nc")
+  )
+
+# perform shapefile to raster conversion
+biomes_raster <- terra::rasterize(biomes, rasta, field = "WWF_MHTNAM")
+
+# get coastline -------------------
+coast <- rnaturalearth::ne_coastline(scale = 110, returnclass = "sf")
+
+# get locations of representative sites ----------------
+sites <- read_rds(here::here("data/df_sites_sub.rds")) |>
+  sf::st_as_sf(coords = c("lon", "lat"), crs = sf::st_crs(biomes))
+
+# Plot map ----------------
 gg <- ggplot() +
   tidyterra::geom_spatraster(data = biomes_raster) +
   geom_sf(data = coast,
           colour = 'black',
           size = 0.05) +
-  geom_sf(data = sites, col = "red") +
+  geom_sf(data = sites, fill = "red", shape = 21) +
   ggrepel::geom_label_repel(data = sites, 
                            aes(label = sitename, geometry = geometry),
                            stat = "sf_coordinates",
@@ -47,7 +51,7 @@ gg <- ggplot() +
       "Inland Water"                                                 = "azure", 
       "Mangroves"                                                    = "violetred", 
       "Mediterranean Forests, Woodlands and Scrub"                   = "orangered3", 
-      "Montane Grasslands and Shrublands"                            = "cadetblue3", 
+      "Montane Grasslands and Shrublands"                            = "steelblue3", 
       "Rock and Ice"                                                 = "azure4", 
       "Temperate Broadleaf and Mixed Forests"                        = "springgreen3", 
       "Temperate Conifer Forests"                                    = "#31A278", 
@@ -64,28 +68,8 @@ gg <- ggplot() +
   ) +
   labs(title = "Biomes", x = "", y = "") + 
   theme(legend.position = "bottom",
-        legend.text = element_text(size = 6),
+        legend.text = element_text(size = 8),
         legend.title = element_blank())
 
-ggsave(here::here("book/images/map_biomes.png"), width = 10, height = 6)
-
-# # biomes_agg <- biomes |>
-# #   group_by(BIOME) |>
-# #   summarise()
-# # 
-# # saveRDS(biomes_agg, file = here::here("data/biomes_agg.rds"))
-# 
-# # biomes_agg <- read_rds(here::here("data/biomes_agg.rds"))
-# 
-# sites <- read_rds(here::here("data/df_sites_sub.rds")) |> 
-#   sf::st_as_sf(coords = c("lon", "lat"), crs = sf::st_crs(biomes))
-# 
-# tm_shape(biomes) +
-#   tm_polygons(fill = "WWF_MHTNAM", 
-#               fill.scale = tm_scale_ordinal(values = "Earth"), 
-#               fill.legend = tm_legend(title = "Biome")) +
-#   # tm_layout(legend.outside.position = "left", legend.outside.size = 0.2) +
-#   tm_shape(sites) +
-#   tm_dots(size = 0.2, col = "red") +
-#   tm_place_legends_right(width = 0.2)
+ggsave(here::here("book/images/map_biomes.png"), width = 12, height = 7)
 
