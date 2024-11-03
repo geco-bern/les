3716a3852e59db1fefdd23bcd579b363d3d67940
Beni Stocker <benjamin.stocker@gmail.com>
2024-11-03 16:20:22 +0100

    added some comments, see XXX

--- analysis/aridityindex_fluxnet.R
+++ analysis/aridityindex_fluxnet.R
@@ -4,19 +4,20 @@ library(lubridate)
 library(readr)
 library(cwd)
 library(gghighlight)
+library(cowplot)
 
 # Get site meta info -----------------
-sites <- readr::read_csv("~/data_2/FluxDataKit/v3.3/zenodo_upload/fdk_site_info.csv") |>
+sites <- readr::read_csv("~/data_2/FluxDataKit/v3.4/zenodo_upload/fdk_site_info.csv") |>
   filter(!(sitename %in% c("MX-Tes", "US-KS3"))) |>  # failed sites
   left_join(
-    readr::read_csv("~/data_2/FluxDataKit/v3.3/zenodo_upload/fdk_site_fullyearsequence.csv"),
+    readr::read_csv("~/data_2/FluxDataKit/v3.4/zenodo_upload/fdk_site_fullyearsequence.csv"),
     by = "sitename"
   ) |> 
   filter(!drop_lecorr) |>  # where no full year sequence was found
   filter(nyears_lecorr >= 3)
 
 # Load data -------------
-path <- "~/data_2/FluxDataKit/v3.3/zenodo_upload/fluxnet/"  # adjust for your own local use
+path <- "~/data_2/FluxDataKit/v3.4/zenodo_upload/fluxnet/"  # adjust for your own local use
 read_onesite <- function(site, path){
   filename <- list.files(path = path, 
                          pattern = paste0("FLX_", site, "_FLUXDATAKIT_FULLSET_DD"), 
@@ -55,7 +56,7 @@ le_to_et <- function(le, tc, patm){
 adf <- ddf |> 
   mutate(
     # convert latent heat flux into mass flux in mm day-1
-    le_mm = le_to_et(LE_CORR, TA_F_MDS, PA_F),
+    le_mm = le_to_et(LE_F_MDS, TA_F_MDS, PA_F),
     pet = 60 * 60 * 24 * cwd::pet(NETRAD, TA_F_MDS, PA_F)
   ) |> 
   mutate(year = year(TIMESTAMP)) |> 
@@ -82,12 +83,15 @@ gg1 <- adf |>
       y = aet
     )
   ) + 
-  geom_point() +
-  gghighlight(sitename %in% c("DE-Hai", "US-Ton", "FI-Hyy", "US-ICh", "AU-How"), 
-              label_key = sitename, 
-              use_direct_label = FALSE) +
+  geom_point(color = "tomato") +
+  gghighlight(
+    sitename %in% c("DE-Hai", "US-Ton", "FI-Hyy", "US-ICh", "AU-How"), 
+    label_key = sitename, 
+    use_direct_label = FALSE,
+    unhighlighted_params = list(color = "grey40")
+  ) +
   geom_label(aes(label = sitename),
-             hjust = 1, vjust = 1, fill = "white", colour = "black", alpha= 0.5) +
+             hjust = 1, vjust = 1, fill = "white", colour = "black", alpha = 0.7) +
   geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
   geom_hline(yintercept = 1, linetype = "dotted") +
   theme_classic() +
@@ -105,12 +109,15 @@ gg2 <- adf |>
       y = aet/prec
     )
   ) + 
-  geom_point() +
-  gghighlight(sitename %in% c("DE-Hai", "US-Ton", "FI-Hyy", "US-ICh", "AU-How"), 
-              label_key = sitename, 
-              use_direct_label = FALSE) +
+  geom_point(color = "tomato") +
+  gghighlight(
+    sitename %in% c("DE-Hai", "US-Ton", "FI-Hyy", "US-ICh", "AU-How"), 
+    label_key = sitename, 
+    use_direct_label = FALSE,
+    unhighlighted_params = list(color = "grey40")
+    ) +
   geom_label(aes(label = sitename),
-             hjust = 1, vjust = 1, fill = "white", colour = "black", alpha= 0.5) +
+             hjust = 1, vjust = 1, fill = "white", colour = "black", alpha = 0.7) +
   geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
   geom_hline(yintercept = 1, linetype = "dotted") +
   theme_classic() +
@@ -128,3 +135,26 @@ plot_grid(
 )
 
 ggsave(here::here("book/images/budyko_fluxnet.png"), width = 8, height = 3.5)
+
+
+# additional plot not used in the book
+gg3 <- adf |> 
+  ggplot(
+    aes(
+      x = pet/prec,
+      y = aet/prec
+    )
+  ) + 
+  geom_point(color = "grey40") +
+  geom_abline(slope = 1, intercept = 0, linetype = "dotted") +
+  geom_hline(yintercept = 1, linetype = "dotted") +
+  theme_classic() +
+  xlim(0, 3) +
+  ylim(0, 1.2) +
+  labs(
+    x = expression(paste("PET/", italic(P))),
+    y = expression(paste("AET/", italic(P)))
+  )
+
+ggsave(here::here("book/images/budyko_only_fluxnet.pdf"), width = 5, height = 3.5)
+
